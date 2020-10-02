676d0ae63b6eb0b92476f89a9589fa9c951b3f23
Oscar Otero <oom@oscarotero.com>
2020-10-02 21:04:39 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# deno-converter
+# nodedeno
+
 Script to convert Node libraries to Deno
+
+- Transform CJS to ESM
+- Transform TypeScript to JavaScript
+- Any dependency is replaced with `./deps.js`
+- Replace some Node global object like `process.env` or `__dirname` to Deno equivalents
+
+## Example:
+
+```js
+const dep = require("my-dependency");
+const otherModule = require("./other-module");
+
+module.exports = function foo() {};
+module.exports.otherModule = function bar() {};
+```
+
+Is converted to:
+
+```js
+import dep from "./deps.js";
+import otherModule from "./other-module.js";
+
+export default function foo() {};
+export const otherModule = function bar() {};
+```
+
+## Usage:
+
+```js
+import { convert } from "https://raw.githubusercontent.com/oscarotero/nodedeno/main/mod.js"
+
+const fromDir = ;
+const toDir = "deno-library/lib";
+const depsFile = "deps.js";
+const ignore =
+
+convert({
+  from: "node-library/lib",
+  to: "deno-library/lib",
+  depsFile: "deps.js",
+  ignoredFiles: [
+    "ignored-file-1.js",
+    "ignored-file-2.js",
+  ],
+  onConvert(file, code) {
+    // Here you can change the code or filename
+    return [file, code];
+  }
+})
+```
---
+++ mod.js
@@ -0,0 +1,139 @@
+import { copy } from "https://deno.land/std/fs/mod.ts";
+import { join } from "https://deno.land/std/path/mod.ts";
+
+const __dirname = `const __dirname = (() => {
+  const { url } = import.meta;
+  const u = new URL(url);
+  return (u.protocol === "file:" ? u.pathname : url).replace(/[/][^/]*$/, '');
+})();`;
+
+const defaults = {
+  ignoredFiles: [],
+  onConvert(file, code) {
+    return [file, code];
+  },
+};
+
+export async function convert(options = {}) {
+  options = { ...defaults, ...options };
+
+  try {
+    await Deno.remove(options.to, { recursive: true });
+  } catch (err) {}
+
+  await copy(options.from, options.to);
+  await convertDirectory(
+    options.to,
+    new Set(options.ignoredFiles),
+    options.onConvert,
+  );
+  await copy(deps_file, join(dest, "deps.js"));
+}
+
+export async function convertDirectory(src, ignored, onConvert) {
+  for await (const entry of Deno.readDir(src)) {
+    let path = join(src, entry.name);
+
+    if (ignored.has(entry.name)) {
+      await Deno.remove(path);
+      continue;
+    }
+
+    if (path.endsWith(".d.ts")) {
+      await Deno.remove(path);
+      continue;
+    }
+
+    if (entry.isDirectory) {
+      await convertDirectory(path);
+      continue;
+    }
+
+    let text = await Deno.readTextFile(path);
+
+    //Transpile .ts => .js
+    if (path.endsWith(".ts")) {
+      const result = await Deno.transpileOnly({
+        [path]: text,
+      });
+
+      await Deno.remove(path);
+      text = result[path].source;
+      text = text.replaceAll("// @ts-expect-error", "");
+      text = text.replace(/\/\/\# sourceMappingURL=.*/, "");
+      path = path.replace(/\.ts$/, ".js");
+    }
+
+    const [file, code] = onConvert(path, convertCode(text));
+
+    await Deno.writeTextFile(file, code);
+
+    if (file !== path) {
+      await Deno.remove(path);
+    }
+  }
+}
+
+export function convertCode(code) {
+  code = code
+    //Remove "use strict" because ES5 modules are always strict
+    .replace(/["']use strict['"]/, "")
+    //Replace default module.exports
+    .replace(/module\.exports\s*=\s*\S/g, (str) => {
+      const postfix = str.slice(-1);
+
+      if (postfix === "{") {
+        return `export ${postfix}`;
+      }
+
+      return `export default ${postfix}`;
+    })
+    //Replace named module.exports
+    .replace(/module\.exports\.(\w+)\s*=\s*\S/g, (str, name) => {
+      const postfix = str.slice(-1);
+
+      return `export const ${name} = ${postfix}`;
+    })
+    //Fix current import
+    .replace(
+      /import\s+({[^}]+}|\S+)\s*from\s*['"]([^'"]+)['"]/g,
+      (str, name, path) => importFrom(name, path),
+    )
+    //Replace require()
+    .replace(
+      /(let|const|var)\s+({[^}]+}|\S+)\s*=\s*require\(['"]([^'"]+)['"]\)/g,
+      (str, prefix, name, path) => importFrom(name, path),
+    )
+    .trimStart();
+
+  //Replace node global objects by Deno equivalents
+  if (code.includes("Buffer.")) {
+    code = `import { Buffer } from "./deps.js";\n${code}`;
+  }
+  if (code.includes("__dirname")) {
+    code = `${__dirname}\n${code}`;
+  }
+  code = code.replace(/process\.env\./g, "Deno.env.");
+
+  return code;
+}
+
+function importFrom(name, path) {
+  //Relative import
+  if (path.startsWith(".")) {
+    if (path.endsWith("..")) { // require("..");
+      path = join(path, "lib/mod.js");
+    } else if (!path.endsWith(".js")) {
+      path = `${path}.js`;
+    }
+
+    return `import ${name} from "${path}";`;
+  }
+
+  //Import dependency
+  if (name.startsWith("{")) {
+    return `import ${name} from "./deps.js";`;
+  }
+
+  return `import { ${name} } from "./deps.js";`;
+}
